ad4065d097beccf8724a3fd8a2151ae34b908dd0
daesoo <daesoo@datanuri.net>
2023-10-30 08:43:13 +0900

    [feat] Init Project

---
+++ src/main/java/com/project/msa/MsaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaProjectApplication.class, args);
+    }
+
+}
